9ccb02e8344c1b01045dfdc5f331bea16f7cbec0
Balazs Horvath <acsipont@gmail.com>
2024-05-23 12:00:44 +0200

    awoo
    Signed-off-by: Balazs Horvath <acsipont@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,28 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.createHuggingFaceLink', async () => {
-        const editor = vscode.window.activeTextEditor;
+  let disposable = vscode.commands.registerCommand(
+    "extension.createHuggingFaceLink",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
 
-        if (editor) {
-            const selections = editor.selections;
-            await editor.edit(editBuilder => {
-                for (const selection of selections) {
-                    const text = editor.document.getText(selection);
-                    const lines = text.split('\n').filter(line => line.trim() !== ''); // Split text into lines and filter out empty lines
-                    for (const line of lines) {
-                        const formattedText = `- [${line}](https://huggingface.co/k4d3/yiff_toolkit/resolve/main/ponyxl_loras_shrunk/${line}?download=true)\n`;
-                        editBuilder.replace(selection, formattedText);
-                    }
-                }
-            });
-        }
-    });
+      if (editor) {
+        const selections = editor.selections;
+        await editor.edit((editBuilder) => {
+          for (const selection of selections) {
+            const text = editor.document.getText(selection);
+            const lines = text.split("\n").filter((line) => line.trim() !== ""); // Split text into lines and filter out empty lines
+            for (const line of lines) {
+              const formattedText = `- [${line}](https://huggingface.co/k4d3/yiff_toolkit/resolve/main/ponyxl_loras_shrunk/${line}?download=true)\n`;
+              editBuilder.replace(selection, formattedText);
+            }
+          }
+        });
+      }
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
